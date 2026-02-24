0140ad345667f852de309da3fd239e5a1197e5e8
dharanimalairaj26@gmail.com <dharanimalairaj26@gmail.com>
2026-02-25 04:37:38 +0530

    server.js

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import http from "http";
+
+import connectDB from "./config/db.js";
+import socketServer from "./socket.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import roomRoutes from "./routes/roomRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import fileRoutes from "./routes/fileRoutes.js";
+
+import "./cron/roomCleanup.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/rooms", roomRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/files", fileRoutes);
+
+const server = http.createServer(app);
+socketServer(server);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
